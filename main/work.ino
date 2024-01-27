b0f176fbe34b4c4ea2f30897109c3e2eb8e405d8
Mike <125614854+E64b@users.noreply.github.com>
2024-01-28 01:07:17 +0300

    Create work.ino

---
+++ main/work.ino
@@ -0,0 +1,54 @@
+#include <Servo.h>
+
+/*SETUP*/
+#define PREPARE_TIME 3000 // Delay after start ms
+#define FLYING_TIME 18000 // Time flight ms
+#define TIME_STEP 20      // Time step for runing and breaking
+#define MAX_RPM 150       // Max rpm 0-100%
+
+Servo PID;
+
+uint8_t maxThrottle = MAX_RPM;
+uint8_t curThrottle = 0;
+uint32_t previousMillis = 0;
+bool start;
+bool breaking = false;
+bool delayStart = true;
+
+void setup() {
+  PID.attach(9, 1000, 2000);
+  PID.write(curThrottle);
+}
+
+void loop() {
+  if (delayStart) {
+    PID.write(0);
+    delay(PREPARE_TIME);
+    start = true;
+  }
+
+  if (start) {
+    for (curThrottle = 0; curThrottle < maxThrottle; curThrottle++) {
+      PID.write(curThrottle);
+      delay(TIME_STEP);
+    }
+
+    uint32_t currentMillis = millis();
+
+    while (millis() - currentMillis <= FLYING_TIME) {
+      PID.write(maxThrottle);
+      breaking = true;
+    }
+
+    if (breaking) {
+      for (curThrottle = maxThrottle; curThrottle > 0; curThrottle--) {
+        PID.write(curThrottle);
+        delay(TIME_STEP);
+      }
+      PID.write(0);
+      breaking = false;
+    }
+  }
+  start = false;
+  delayStart = false;
+}
